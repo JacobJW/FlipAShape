22b40351bb4ba3b7108cc90ecc89b2ddc34b468c
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2016-07-21 18:41:35 -0700

    Update FlipAShapeTester.py

--- FlipAShapeTester.py
+++ FlipAShapeTester.py
@@ -6,41 +6,44 @@ def flip_it ():
         
         base = input("How many units do you want the base to be? \n")
         print("\n")
-           
-        if base.isdigit() == False or int(base) < 2:    
+        try:   
+            if base.isdigit() == False or int(base) < 2: 
                 print("Sorry, that's not a valid input. Please use only numbers and make the base larger than 1. \n")
-
+            break
+        except ValueError:
+            print("Sorry, that's not a valid input. Please use only numbers and make the base larger than 1. \n")
+                
         elif base.isdigit() == True and int(base) >= 2:
             initial = False
-
-        run = True
+            
+    run = True
         
-        if run == True:
-           
-            # Converts base into an int for usage later on in multiplication
-            base = int(base)
+    if run == True:
+       
+        # Converts base into an int for usage later on in multiplication
+        base = int(base)
+                
+        Triangle = []
+        
+        # Creates the base of #'s and makes each row have 1 less # , plus indents it by one space to keep triangle format
+        
+        for num in range(base, 0, -1):
+            Triangle.insert(0, [' #' * num])
+            Triangle[0].insert(0, ' ' * (base - num))
+
+        # Joins the separate strings of spaces and #'s together 
+        for num in range(0, base):
+            Triangle[num] = ''.join(Triangle[num])
                     
-            Triangle = []
-            
-            # Creates the base of #'s and makes each row have 1 less # , plus indents it by one space to keep triangle format
-            
-            for num in range(base, 0, -1):
-                Triangle.insert(0, [' #' * num])
-                Triangle[0].insert(0, ' ' * (base - num))
+        for row in Triangle:
+            print(row)
+        
+        print(" \nFLIPPING . . . \n")
 
-            # Joins the separate strings of spaces and #'s together 
-            for num in range(0, base):
-                Triangle[num] = ''.join(Triangle[num])
-                        
-            for row in Triangle:
-                print(row)
-            
-            print(" \nFLIPPING . . . \n")
-
-            # Since each row is its own list, to flip we print the rows in reverse.
-            
-            for num in range(base -1, -1, -1):
-                print(Triangle[num])
+        # Since each row is its own list, to flip we print the rows in reverse.
+        
+        for num in range(base -1, -1, -1):
+            print(Triangle[num])
 
         
     
